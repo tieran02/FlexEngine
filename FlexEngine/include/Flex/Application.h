b894884afd322042985e4772afb041a3a2ff0e82
Unknown <tieran.wightman@gmail.com>
2019-01-18 12:04:16 +0000

    Added Window Events

--- FlexEngine/include/Flex/Application.h
+++ FlexEngine/include/Flex/Application.h
@@ -2,6 +2,7 @@
 #include "Core.h"
 #include "Window.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Flex {
 	class Application
@@ -10,8 +11,13 @@ namespace Flex {
 		FLEX_API Application();
 		FLEX_API virtual ~Application();
 		FLEX_API void Run();
+
+		FLEX_API void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent e);
+
 		std::unique_ptr<Window> m_window;
+		bool m_running{ true };
 	};
 
 	//TO be defined in a client
--- FlexEngine/src/Flex/Application.cpp
+++ FlexEngine/src/Flex/Application.cpp
@@ -10,6 +10,7 @@ namespace Flex {
 	Application::Application()
 	{
 		m_window = std::unique_ptr<Window>(Window::Create());
+		m_window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
 	}
 
 
@@ -19,9 +20,23 @@ namespace Flex {
 
 	void Application::Run()
 	{
-		while (true)
+		while (m_running)
 		{
 			m_window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(std::bind(&Application::OnWindowClose, this, std::placeholders::_1));
+		FL_LOG_CORE_INFO("{0}", e);
+
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent e)
+	{
+		m_running = false;
+		return true;
 	}
 } 
